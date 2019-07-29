85ec18c65206c4d749b892770cd7913fd094c8b2
goliatone <burgosemi@gmail.com>
2019-07-28 21:00:52 -0700

    remove scheduler test

--- test/scheduler-test.js
+++ test/scheduler-test.js
@@ -15,26 +15,8 @@ test('Scheduler has defaults', t => {
     t.end();
 });
 
-test('Scheduler _isTaskId can match the right id format', t => {
-    const scheduler = new Scheduler({
-        autoinitialize: false
-    });
-    t.ok(scheduler._isTaskId('scheduler:tasks:task01'));
-    t.ok(scheduler._isTaskId('scheduler:tasks:ac8c7416-4144-40d3-a8d2-b3f1b454c10c'));
-    t.notOk(scheduler._isTaskId('scheduler:tasks:ac8c7416-4144-40d3-a8d2-b3f1b454c10c:ttl'));
-    t.notOk(scheduler._isTaskId('scheduler:tasks:task01:ttl'));
-    t.end();
-});
 
-test('Scheduler _getTaskIdFromExpireKey can get a task id from an expired key', t => {
-    const scheduler = new Scheduler({
-        autoinitialize: false
-    });
-    const key = 'scheduler:tasks:ac8c7416-4144-40d3-a8d2-b3f1b454c10c:ttl';
-    const expected = 'scheduler:tasks:ac8c7416-4144-40d3-a8d2-b3f1b454c10c';
-    t.equals(scheduler._getTaskIdFromExpireKey(key), expected);
-    t.end();
-});
+
 
 test('Scheduler can add new tasks', t => {
     const scheduler = new Scheduler({
@@ -42,13 +24,13 @@ test('Scheduler can add new tasks', t => {
         clientFactory,
     });
 
-    scheduler.add({
+    scheduler.addTask({
 
     });
     t.end();
 });
 
-test('Scheduler can add remove tasks', t => {
+test.skip('Scheduler can add remove tasks', t => {
     const scheduler = new Scheduler({
         autoinitialize: true,
         clientFactory,
