df1a5a6ba823cf43537dbda6016f7d17e6eb1619
goliatone <burgosemi@gmail.com>
2021-04-29 21:56:26 -0700

    update example

--- examples/queue.js
+++ examples/queue.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const logger = require('./log');
-const log = logger('queue ');
+const log = logger('queue');
 
 const Scheduler = require('../lib/scheduler');
 
@@ -15,21 +15,20 @@ const scheduler = new Scheduler({
 });
 
 const urls = [
-    // 'https://www.splitbrain.org/services/ots',
-    // 'https://github.com/mozilla/readability',
-    // 'https://www.elastic.co/products/apm',
-    // 'https://github.com/kalliope-project/kalliope/',
-    // 'https://docs.droplit.io/docs/containers',
-
-    // 'http://wiki.psuter.ch/doku.php?id=solve_raspbian_sd_card_corruption_issues_with_read-only_mounted_root_partition',
-    // 'https://github.com/awnumar/memguard/blob/master/README.md',
-    // 'https://github.com/parro-it/libui-node',
-    // 'https://github.com/ddollar/forego',
-    // 'https://github.com/wpmed92/backpropaganda',
-    // 'https://github.com/outline/outline',
-    // 'https://itnext.io/going-multithread-with-node-js-492258ba32cf',
-    // 'http://www.bloomberg.com/news/articles/2016-05-24/as-zenefits-stumbles-gusto-goes-head-on-by-selling-insurance',
-    'http://www.goliatone.com/blog/2015/11/13/raspberry-pi-nodejs-open-zwave/',
+    'http://www.goliatone.com',
+    // 'https://www.splitbrain.org',
+    // 'https://github.com/mozilla',
+    // 'https://www.elastic.co',
+    // 'https://github.com',
+    // 'https://docs.droplit.io',
+    // 'http://wiki.psuter.ch',
+    // 'https://github.com',
+    // 'https://github.com',
+    // 'https://github.com',
+    // 'https://github.com',
+    // 'https://github.com',
+    // 'https://itnext.io',
+    // 'http://www.bloomberg.com',
 ];
 
 // (async _ => {
@@ -48,23 +47,31 @@ const urls = [
 addTaskWorker();
 
 function addTaskWorker() {
+    if (urls.length === 0) return log('Done!');
+
     counter++;
 
     const index = scheduler.backend.getUid();
     log('Create task %s, ID: "%s"', counter, index);
 
-    scheduler.addTask({
+    let task = {
         id: index,
-        // maxRuns: 5,
-        maxRuns: 1,
+        queue: 'link.archive',
+        maxRuns: randomInt(1, 4),
         maxTries: 5,
         // expire: 2000,
         data: {
             url: urls.pop()
         }
-    }).then(_ => {
-        if (urls.length > 0) addTaskWorker();
-        else log('Done!');
-        // setTimeout(addTaskWorker, randomInt(200, 1000));
+    };
+
+    log('Task has a max run of: %s', task.maxRuns);
+
+    scheduler.addTask(task).then(_ => {
+        setTimeout(addTaskWorker, randomInt(200, 1000));
     });
+}
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
