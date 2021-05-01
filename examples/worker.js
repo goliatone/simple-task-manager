a1e740f5ac0b57d474fcdbc41ad970cc09f339f4
goliatone <burgosemi@gmail.com>
2021-04-30 22:37:42 -0700

    update example

--- examples/worker.js
+++ examples/worker.js
@@ -7,6 +7,7 @@ const log = logger('worker');
 
 const worker = new Worker({
     queue: 'link.archive',
+    logger: logger('manager'),
     backend: {
         host: 'localhost',
         port: 6379
@@ -15,48 +16,53 @@ const worker = new Worker({
 
 
 worker
+    .on('task.added', task => {
+        log.info('------------------------------');
+        log.info('----- TASK ADDED ----');
+        log.info('* Task has been ran %s before.', task.runs);
+        log.info('* It should run %s times.', task.maxRuns);
+        log.info('* Pulled queue: "%s" task id "%s"', 'tasks:active', task.id);
+        console.log('');
+    })
     .on('task.ran', task => {
-        log('----- task ran ----');
-        log('Job complete...\n');
-        log('- This "task" has run "%s" times', task.runs);
-        log('- "job" ran "%s" times with %s errors and %s failures', worker.runs - worker.errors, worker.errors, worker.failed);
-        // log('- execution took %sms', timeout);
-        log('----------------------\n');
-    }).on('task.added', task => {
-        log('----- task added ----');
-        log('! on task added');
-        log('* Task has been ran %s before.', task.runs);
-        log('* Pulled queue: "%s" task id "%s"', 'tasks:active', task.id);
+        log.info('------------------------------');
+        log.info('----- TASK RAN ----');
+        log.info('Job complete...\n');
+        log.info('- Task has run "%s/%s" times with "%s" errors', task.runs, task.maxRuns, task.errorCount);
+        log.info('- "job" ran "%s" times with %s errors and %s failures', worker.runs - worker.errors, worker.errors, worker.failed);
+        log.info('------------------------------');
+        log.info('------------------------------');
+        console.info('');
     }).on('task.error', task => {
-        log('\t----- ERROR ----');
-        log('\tTask "%s" failed...', task.id);
-        log('\tErrors: %s/%s', task.errorCount, task.maxTries);
-        // log('\tserialized: %s', task.serialize());
-        log('-------------------\n');
+        log.info('------------------------------');
+        log.info('----- TASK ERROR ----');
+        log.info('* Task "%s" failed...', task.id);
+        log.info('* Errors: %s/%s', task.errorCount, task.maxTries);
+        log.info('-------------------\n');
     })
     .run(taskHandler);
 
 async function taskHandler(task) {
-    log('-------- WORKER --------');
+    log.info('-------- WORKER --------');
     if (!task.data) {
-        log('No task data!!');
-        log('%j', task);
+        log.info('No task data!!');
+        log.info('%j', task);
         process.exit(1);
     }
 
-    log('Start job with id: %s...', task.id);
-    log('Prepare to summarize url %s...', task.data.url);
+    log.info('Start job with id: %s...', task.id);
+    log.info('Prepare to summarize url %s...', task.data.url);
 
     const timeout = randomInt(100, 2000);
 
     if (timeout > 1400) {
-        log('Timed out error...');
+        log.error('Timed out error...');
         throw new Error(`${task.id}`, 504);
     }
 
     return new Promise((resolve, reject) => {
         setTimeout(_ => {
-            log('Download done!');
+            log.info('Download done!');
             resolve();
         }, timeout);
     });
