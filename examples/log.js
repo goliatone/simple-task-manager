a8bfb6d69a0c327f565a5e65ec1a9b31ffe28483
goliatone <burgosemi@gmail.com>
2021-04-30 22:38:01 -0700

    add: log example error and info methods

--- examples/log.js
+++ examples/log.js
@@ -1,22 +1,38 @@
 'use strict';
 
 function logger(name) {
-    return function $log(...args) {
-        let time = new Date();
-        time = '[' +
-            ('0' + time.getHours()).slice(-2) + ':' +
-            ('0' + time.getMinutes()).slice(-2) + ':' +
-            ('0' + time.getSeconds()).slice(-2) + ':' +
-            ('00' + time.getMilliseconds()).slice(-3) + ']';
+    return {
+        info: function $info(...args) {
+            let time = new Date();
+            time = '[' +
+                ('0' + time.getHours()).slice(-2) + ':' +
+                ('0' + time.getMinutes()).slice(-2) + ':' +
+                ('0' + time.getSeconds()).slice(-2) + ':' +
+                ('00' + time.getMilliseconds()).slice(-3) + ']';
 
-        if (typeof args[0] === 'string') {
-            args = ['%s %s |\t' + args.shift(), time, name, ...args];
+            if (typeof args[0] === 'string') {
+                args = ['%s %s |\t' + args.shift(), time, name, ...args];
+            }
+
+            console.log.apply(null, args);
+        },
+        error: function $error(...args) {
+            let time = new Date();
+            time = '[' +
+                ('0' + time.getHours()).slice(-2) + ':' +
+                ('0' + time.getMinutes()).slice(-2) + ':' +
+                ('0' + time.getSeconds()).slice(-2) + ':' +
+                ('00' + time.getMilliseconds()).slice(-3) + ']';
+
+            if (typeof args[0] === 'string') {
+                args = ['%s %s |\t' + args.shift(), time, name, ...args];
+            }
+
+            console.error.apply(null, args);
         }
-
-        console.log.apply(null, args);
     }
 }
 
 module.exports = function(name) {
-    return logger('name');
+    return logger(name);
 };
