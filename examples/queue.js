6caa39d9e2d2d89ce6d8214b2d61b9302887d480
goliatone <burgosemi@gmail.com>
2021-05-01 21:37:11 -0700

    udpate example

--- examples/queue.js
+++ examples/queue.js
@@ -31,33 +31,87 @@ const urls = [
     // 'http://www.bloomberg.com',
 ];
 
-// (async _ => {
-//     console.log('get active tasks');
-//     let ids = await scheduler.getActiveTasks();
-//     let tasks = await scheduler.getTasks(ids);
-//     console.log(ids)
-//     console.log(tasks)
-//     for (let task of tasks) {
-//         console.log(task.id);
-//         console.log(task.key);
-//         console.log(task.ttlKey);
-//     }
-// })();
+(async _ => {
+    console.info('get active tasks');
+    let all, active, failed, completed, tasks;
 
-addTaskWorker();
+    try {
+        all = await scheduler.getAllTaskIds();
+    } catch (error) {
+        console.error('error all');
+        console.error(error);
+        process.exit();
+    }
+
+    try {
+        active = await scheduler.getActiveTasks();
+    } catch (error) {
+        console.error('error active');
+        console.error(error);
+        process.exit();
+    }
+
+    try {
+        failed = await scheduler.getFailedTasks();
+    } catch (error) {
+        console.error('error failed');
+        console.error(error);
+        process.exit();
+    }
+
+    try {
+        completed = await scheduler.getCompletedTasks();
+    } catch (error) {
+        console.error('error completed');
+        console.error(error);
+        process.exit();
+    }
+
+    try {
+        tasks = await scheduler.getTasks(all);
+    } catch (error) {
+        console.error('error tasks');
+        console.error(error);
+        process.exit();
+    }
+
+    console.log('all', all.length);
+    console.log('active', active.length);
+    console.log('failed', failed.length);
+    console.log('completed', completed.length);
+
+
+    if (all.length > 0) {
+        console.info(all)
+        console.info(tasks)
+
+        for (let task of tasks) {
+            console.info(task.id);
+            console.info(task.key);
+            console.info(task.ttlKey);
+        }
+    }
+
+    scheduler.backend.client.flushall().then(addTaskWorker);
+
+})();
+
+
+// addTaskWorker();
 
 function addTaskWorker() {
-    if (urls.length === 0) return log('Done!');
+    if (urls.length === 0) return log.info('Done!');
 
     counter++;
 
     const index = scheduler.backend.getUid();
-    log('Create task %s, ID: "%s"', counter, index);
+    log.info('Create task %s, ID: "%s"', counter, index);
 
     let task = {
         id: index,
         queue: 'link.archive',
-        maxRuns: randomInt(1, 4),
+        schedule: '*/2 * * * *',
+        maxRuns: randomInt(2, 4),
         maxTries: 5,
         // expire: 2000,
         data: {
@@ -65,11 +119,11 @@ function addTaskWorker() {
         }
     };
 
-    log('Task has a max run of: %s', task.maxRuns);
+    log.info('Task has a max run of: %s', task.maxRuns);
 
     scheduler.addTask(task).then(_ => {
         setTimeout(addTaskWorker, randomInt(200, 1000));
-    });
+    }).catch(console.error);
 }
 
 function randomInt(min, max) {
